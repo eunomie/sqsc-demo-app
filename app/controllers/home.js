322381958c23b411f767531956ef378e1f425569
Romain Lenoel <romain.lenoel@gmail.com>
2017-08-18 18:21:36 +0200

    computation parameters are sent from client

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,8 +6,6 @@ const amqp = require('amqplib');
 
 const processingQueueName = process.env.PROCESSING_QUEUE_NAME || "processingQueue";
 
-const precision = 100;
-const computeWidth = 10;
 const imageWidth = 900;
 const imageHeight= 600;
 let channel;
@@ -26,15 +24,20 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/launch', function(req, res) {
-  for (var i = 0; i < imageWidth / computeWidth; i++) {
-    publishNewMessage(i * computeWidth);
+router.post('/launch', function(req, res) {
+  let params = req.body;
+  console.log(params)
+  for (var i = 0; i < imageWidth / params.stepX; i++) {
+    publishNewMessage(i * params.stepX, 
+      parseInt(params.precision), 
+      parseInt(params.stepX), 
+      parseInt(params.stepY));
     socketIO.emit('compute_task_created');
   }
   res.sendStatus(200);
 });
 
-async function publishNewMessage(col) {
+async function publishNewMessage(col, precision, stepX, stepY) {
 
   const mandelbrotData = {
     x: 0,
@@ -44,9 +47,9 @@ async function publishNewMessage(col) {
     width: imageWidth,
     height: imageHeight,
     step: col,
-    stepX : computeWidth,
-    stepY : imageHeight,
-    iter: precision
+    stepX : stepX || 10,
+    stepY : stepY || imageHeight,
+    iter: precision || 10
   };
 
   await channel.assertQueue(processingQueueName, {
